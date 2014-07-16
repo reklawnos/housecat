fa6de332bcfb688c30d4d9ce014e45d19e8fde96
Walker Henderson <wjahenderson@gmail.com>
2014-07-16 02:12:04 -0500

    Now properly parses all currently available tokens

--- src/housecat.rs
+++ src/housecat.rs
@@ -23,11 +23,16 @@ enum Token {
 	Var,                // 'var'
 	// Symbols
 	Eof,                // End of file
-
+	Colon,              // :
+	OpenBrac,           // {
+	CloseBrac,          // }
+	OpenParen,          // (
+	CloseParen,         // )
 	// User values
 	Ident(Box<String>), // ex. foo, bar
 	Int(int),           // ex. 1324, -43
 	Float(f64),         // ex. 1.3, -34.432e-4
+	String(Box<String>),
 	// Binary ops
 	Add,                // '+'
 	Sub,                // '-'
@@ -35,26 +40,78 @@ enum Token {
 	Div,                // '/'
 }
 
-enum ParseTypes {
+enum ParseType {
 	PtName, //ident or keyword
 	PtFloat,
 	PtInt,
+	PtString,
 	PtSkip,
+	PtColon,
+	PtOpenBrac,
+	PtCloseBrac,
+	PtOpenParen,
+	PtCloseParen,
 	PtComment,
 	PtBinOp
+}
+
+fn decide_token(parse_type: ParseType, section: &str) -> Token {
+	match parse_type {
+		PtName => {
+			match section {
+				"func" => Func,
+				"var" => Var,
+				s => {
+					Ident(box s.to_string())
+				}
+			}
+		}
+		PtFloat => Float(from_str(section).unwrap()),
+		PtInt => Int(from_str(section).unwrap()),
+		//TODO: add support for escape characters
+		PtString => {
+			let mut ns = section.to_string().clone();
+			//remove start and end character
+			ns.pop_char();
+			ns.shift_char();
+			String(box ns)
+		},
+		PtColon => Colon,
+		PtOpenBrac => OpenBrac,
+		PtCloseBrac => CloseBrac,
+		PtOpenParen => OpenParen,
+		PtCloseParen => CloseParen,
+		PtBinOp => {
+			match section {
+				"+" => Add,
+				"-" => Sub,
+				"*" => Mul,
+				"/" => Div,
+				_ => fail!("Unknown binary op")
+			}
+		}
+		_ => fail!("not implemented")
+	}
 }
 
 fn do_file_parse(path: &Path) {
 	let token_specs = [
 		(PtName, regex!(r"^[A-z]\w*")),
-		(PtFloat, regex!(r"^-?[0-9]*\.?[0-9]+(?:e[-+]?[0-9]+)?")),
+		(PtFloat, regex!(r"^-?[0-9]*\.[0-9]+(?:e[-+]?[0-9]+)?")),
 		(PtInt, regex!(r"^-?[0-9]+")),
+		(PtString, regex!("^\"(?:[^\"\\\\]|\\\\.)*\"")),
+		(PtColon, regex!(r"^:")),
+		(PtOpenBrac, regex!(r"^\{")),
+		(PtCloseBrac, regex!(r"^\}")),
+		(PtOpenParen, regex!(r"^\(")),
+		(PtCloseParen, regex!(r"^\)")),
+		(PtBinOp, regex!(r"^[+*/-]")),
 		(PtSkip, regex!(r"^\s")),
 		(PtComment, regex!(r"^#"))
 	];
 
 	let mut file = BufferedReader::new(File::open(path));
-	let mut _result : Vec<Token> = Vec::new();
+	let mut result : Vec<Token> = Vec::new();
 
 	for (line_index, l) in file.lines().enumerate() {
 		println!("------------lexing line {}", line_index);
@@ -80,6 +137,14 @@ fn do_file_parse(path: &Path) {
 				let cl = line.clone();
 				let res = cl.as_slice().slice(start, end);
 				println!("The result at position {} is: '{}'", pos, res);
+				//Skip over whitespace
+				match parse_type {
+					PtSkip => {},
+					_ => {
+						let new_token = decide_token(parse_type, res);
+						result.push(new_token);
+					}
+				}
 				//Push the column index to the end of what we just read
 				col += end;
 				line = line.as_slice().slice_from(end).to_string();
@@ -99,6 +164,27 @@ fn do_file_parse(path: &Path) {
 					line.as_slice().char_at(0));
 				return;
 			}
+		}
+	}
+
+	for r in result.iter() {
+		match r {
+			&Func => println!("Func"),
+			&Var => println!("Var"),
+			&Ident(ref s) => println!("Ident({})", s),
+			&Float(f) => println!("Float({})", f),
+			&Int(i) => println!("Int({})", i),
+			&String(ref s) => println!("String({})", s),
+			&Colon => println!("Colon"),
+			&OpenBrac => println!("OpenBrac"),
+			&CloseBrac => println!("CloseBrac"),
+			&OpenParen => println!("OpenParen"),
+			&CloseParen => println!("CloseParen"),
+			&Add => println!("Add"),
+			&Sub => println!("Sub"),
+			&Div => println!("Div"),
+			&Mul => println!("Mul"),
+			_ => fail!("not implemented")
 		}
 	}
 }
