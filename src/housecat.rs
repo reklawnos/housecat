9696e1fcd78e33ac894800ff4b9ed2a3ffda6641
Walker Henderson <wjahenderson@gmail.com>
2014-07-16 20:40:33 -0500

    Refactored code a bit

--- src/housecat.rs
+++ src/housecat.rs
@@ -2,18 +2,40 @@
 extern crate regex;
 #[phase(plugin)] extern crate regex_macros;
 
-//use std::io;
+use regex::Regex;
 use std::io::BufferedReader;
 use std::io::File;
 use std::os;
 
 fn main() {
 	let args = os::args();
+	let token_specs = vec!(
+		(PtName, regex!(r"^[A-z]\w*")),
+		(PtFloat, regex!(r"^-?[0-9]*\.[0-9]+(?:e[-+]?[0-9]+)?")),
+		(PtInt, regex!(r"^-?[0-9]+")),
+		(PtString, regex!("^\"(?:[^\"\\\\]|\\\\.)*\"")),
+		(PtColon, regex!(r"^:")),
+		(PtOpenBrac, regex!(r"^\{")),
+		(PtCloseBrac, regex!(r"^\}")),
+		(PtOpenParen, regex!(r"^\(")),
+		(PtCloseParen, regex!(r"^\)")),
+		(PtBinOp, regex!(r"^[+*/-]")),
+		(PtSkip, regex!(r"^\s")),
+		(PtComment, regex!(r"^#"))
+	);
 	if args.len() <= 1 {
 		//do_repl();
 	} else {
 		let path = Path::new(args.get(1).as_slice());
-		do_file_parse(&path);
+		let result = do_file_parse(&path, &token_specs);
+		for r in result.unwrap().iter() {
+			match r {
+				&(ref token, line_no, col_no) => {
+					let tok_string = token.to_string();
+					println!("{}, {}:{}", tok_string, line_no + 1, col_no + 1);
+				}
+			}
+		}
 	}
 }
 
@@ -38,6 +60,29 @@ enum Token {
 	Sub,                // '-'
 	Mul,                // '*'
 	Div,                // '/'
+}
+
+impl Token {
+	fn to_string(&self) -> String {
+		match self {
+			&Func => format!("Func"),
+			&Var => format!("Var"),
+			&Ident(ref s) => format!("Ident({})", s),
+			&Float(f) => format!("Float({})", f),
+			&Int(i) => format!("Int({})", i),
+			&String(ref s) => format!("String({})", s),
+			&Colon => format!("Colon"),
+			&OpenBrac => format!("OpenBrac"),
+			&CloseBrac => format!("CloseBrac"),
+			&OpenParen => format!("OpenParen"),
+			&CloseParen => format!("CloseParen"),
+			&Add => format!("Add"),
+			&Sub => format!("Sub"),
+			&Div => format!("Div"),
+			&Mul => format!("Mul"),
+			_ => fail!("not implemented")
+		}
+	}
 }
 
 enum ParseType {
@@ -94,38 +139,22 @@ fn decide_token(parse_type: ParseType, section: &str) -> Token {
 	}
 }
 
-fn do_file_parse(path: &Path) {
-	let token_specs = [
-		(PtName, regex!(r"^[A-z]\w*")),
-		(PtFloat, regex!(r"^-?[0-9]*\.[0-9]+(?:e[-+]?[0-9]+)?")),
-		(PtInt, regex!(r"^-?[0-9]+")),
-		(PtString, regex!("^\"(?:[^\"\\\\]|\\\\.)*\"")),
-		(PtColon, regex!(r"^:")),
-		(PtOpenBrac, regex!(r"^\{")),
-		(PtCloseBrac, regex!(r"^\}")),
-		(PtOpenParen, regex!(r"^\(")),
-		(PtCloseParen, regex!(r"^\)")),
-		(PtBinOp, regex!(r"^[+*/-]")),
-		(PtSkip, regex!(r"^\s")),
-		(PtComment, regex!(r"^#"))
-	];
-
+fn do_file_parse(path: &Path, token_specs: &Vec<(ParseType, Regex)>) -> Option<Box<Vec<(Token, uint, uint)>>> {
 	let mut file = BufferedReader::new(File::open(path));
-	let mut result : Vec<Token> = Vec::new();
+	let mut result : Box<Vec<(Token, uint, uint)>> = box Vec::new();
 
 	for (line_index, l) in file.lines().enumerate() {
-		println!("------------lexing line {}", line_index);
 		let mut line = l.unwrap();
 		let mut col = 0u;
 		while line.len() > 0 {
 			let mut found_token = false;
 			let mut found_comment = false;
-			println!("----------this is one go through")
 			for &(parse_type, ref re) in token_specs.iter() {
 				let pos = match re.find(line.as_slice()) {
 					Some(range) => range,
 					None => continue
 				};
+				//Skip the rest of the line if we found a comment
 				match parse_type {
 					PtComment => {
 						found_comment = true;
@@ -136,13 +165,12 @@ fn do_file_parse(path: &Path) {
 				let (start,end) = pos;
 				let cl = line.clone();
 				let res = cl.as_slice().slice(start, end);
-				println!("The result at position {} is: '{}'", pos, res);
 				//Skip over whitespace
 				match parse_type {
 					PtSkip => {},
 					_ => {
 						let new_token = decide_token(parse_type, res);
-						result.push(new_token);
+						result.push((new_token, line_index, col));
 					}
 				}
 				//Push the column index to the end of what we just read
@@ -157,34 +185,13 @@ fn do_file_parse(path: &Path) {
 			}
 			//No token was found, which means that something was invalid
 			if !found_token {
-				//println!("line is {}", line);
 				println!("Lexing failure: unrecognized symbol at line {}, column {}: '{}'",
 					line_index,
 					col + 1,
 					line.as_slice().char_at(0));
-				return;
+				return None;
 			}
 		}
 	}
-
-	for r in result.iter() {
-		match r {
-			&Func => println!("Func"),
-			&Var => println!("Var"),
-			&Ident(ref s) => println!("Ident({})", s),
-			&Float(f) => println!("Float({})", f),
-			&Int(i) => println!("Int({})", i),
-			&String(ref s) => println!("String({})", s),
-			&Colon => println!("Colon"),
-			&OpenBrac => println!("OpenBrac"),
-			&CloseBrac => println!("CloseBrac"),
-			&OpenParen => println!("OpenParen"),
-			&CloseParen => println!("CloseParen"),
-			&Add => println!("Add"),
-			&Sub => println!("Sub"),
-			&Div => println!("Div"),
-			&Mul => println!("Mul"),
-			_ => fail!("not implemented")
-		}
-	}
+	Some(result)
 }
